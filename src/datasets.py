f8aca61d85cac300982015168dd36afd065dd294
Morris Alper <morrisalp@gmail.com>
2022-04-15 17:47:40 -0700

    add nikud Dataset object

--- src/datasets.py
+++ src/datasets.py
@@ -2,7 +2,8 @@ from random import random
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
-from hebrew_utils import NIKUD, strip_nikud, YUD, VAV, YV
+from hebrew_utils import NIKUD, strip_nikud, YUD, VAV, YV, haser_male2target
+import numpy as np
 
 def make_male_labels(text_n, text_non):
     out = ''
@@ -27,7 +28,7 @@ def make_male_labels(text_n, text_non):
 
 class KtivMaleDataset(Dataset):
 
-    def __init__(self, fn='./data/processed/ktiv_male.csv', tokenizer=None, split=None, val_size=0.1):
+    def __init__(self, fn='./data/processed/ktiv_male.csv', split=None, val_size=0.1):
         self.df = pd.read_csv(fn)
         # random shuffle (with fixed seed) so val split is not biased:
         self.df = self.df.sample(self.df.shape[0], random_state=0)
@@ -79,6 +80,52 @@ class KtivMaleCollator:
         
         return {**X, 'labels': y}
 
+
+class NikudDataset(Dataset):
+
+    def __init__(self, fn='./data/processed/nikud.csv', split=None, val_size=0.1):
+        self.df = pd.read_csv(fn)
+        # random shuffle (with fixed seed) so val split is not biased:
+        self.df = self.df.sample(self.df.shape[0], random_state=0)
+
+        if split is not None:
+            assert split in ['train', 'val']
+            N_TRAIN = int(self.df.shape[0] * (1 - val_size))
+            N_VAL = self.df.shape[0] - N_TRAIN
+            if split == 'train':
+                self.df = self.df.head(N_TRAIN)
+            else:
+                self.df = self.df.tail(N_VAL)
+
+    def __len__(self):
+        return self.df.shape[0]
+    
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+
+        haser = row.text # ktiv haser: has nikud
+        male = strip_nikud(row.text) # ktiv male: no nikud
+        
+        return haser, male
+
+class NikudCollator:
+
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def collate(self, batch):
+        male_texts = [male for haser, male in batch]
+        raw_targets = [haser_male2target(haser, male) for haser, male in batch]
+        toks = self.tokenizer(male_texts, padding='longest', truncation=True, return_tensors='pt')
+        LEN = len(toks.input_ids[0]) # includes [CLS] & [SEP]
+        
+        def pad_target(tgt):
+            N_PADDING = max(0, 1 + LEN - tgt.shape[0] - 2)
+            return np.pad(tgt, ((1, N_PADDING), (0, 0)))[:LEN]
+        
+        return toks, torch.tensor(np.stack([pad_target(t) for t in raw_targets])).float()
+
+
 if __name__ == '__main__':
 
     from transformers import CanineTokenizer
@@ -97,3 +144,16 @@ if __name__ == '__main__':
     print(out)
     for k in out:
         print(k, out[k].shape)
+    
+    print('-' * 50)
+
+    n = NikudDataset(split='val')
+    print('LEN:', len(n))
+    nc = NikudCollator(tokenizer)
+
+    In = iter(n)
+
+    batchn = [next(In), next(In), next(In)]
+
+    outn = nc.collate(batchn)
+    print(outn)
