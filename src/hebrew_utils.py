e80bee6cbe66ebf88310d81158ddf2f6293d7025
Morris Alper <morrisalp@gmail.com>
2022-04-15 17:30:07 -0700

    add nikud target conversion to hebrew utils

--- src/hebrew_utils.py
+++ src/hebrew_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Unicode codepoints for nikud:
 # NOTE: Some of these are extended nikud which we will not use
 # 1456 HEBREW POINT SHEVA
@@ -53,3 +55,97 @@ def text_contains_abg(text):
 YUD = 'י'
 VAV = 'ו'
 YV = YUD + VAV
+
+
+### utilities for converting (haser, male) text pairs into input & target for nikud model: ###
+# haser: includes nikud, but not extra yuds/vavs
+
+def align_haser_male(haser, male):
+    '''Input: pairs of texts in ktiv haser (with nikud) and ktiv male
+    Output: list of pairs (c1, c2) of characters; c1 in haser, c2 in male'''
+    i = 0
+    j = 0
+    output = []
+    while i < len(haser) and j < len(male):
+        if i >= len(haser):
+            output += [('', male[j])]
+            j += 1
+        elif j >= len(male):
+            output += [(haser[i], '')]
+            i += 1
+        elif haser[i] == male[j]:
+            output += [(haser[i], male[j])]
+            i += 1
+            j += 1
+        elif haser[i] in NIKUD:
+            output += [(haser[i], '')]
+            i += 1
+        else:
+            output += [('', male[j])]
+            j += 1
+            
+    return output
+
+
+def chunk_haser_male(haser, male):
+    '''uses alignment from previous method to split text into chunks
+    outputs list of chunks, one chunk has format: (str, bool)
+    str: Hebrew consonant with vowel(s) attached
+    bool: True iff letter should be deleted (i.e. extra yud/vav)'''
+
+    aligned = align_haser_male(haser, male)
+    
+    chunks = []
+    del_flags = []
+    cur_chunk = ''
+    
+    for c1, c2 in aligned:
+        if c1 == c2:
+            if cur_chunk != '':
+                chunks.append(cur_chunk)
+                del_flags.append(False)
+            cur_chunk = ''
+            cur_chunk += c1
+        elif c1 == '':
+            if cur_chunk != '':
+                chunks.append(cur_chunk)
+                del_flags.append(False)
+            cur_chunk = ''
+            chunks.append(c2)
+            del_flags.append(True)
+        else:
+            cur_chunk += c1
+    
+    if cur_chunk != '':
+        chunks.append(cur_chunk)
+        del_flags.append(False)
+    
+    return list(zip(chunks, del_flags))
+
+def chunk2target(chunk):
+    '''turns chunks from previous method into multilabel targets for nikud model'''
+    
+    text, del_flag = chunk
+    
+    nikkud_list = [
+        int(chr(n) in text)
+        for n in range(NIKUD_START_ORD, NIKUD_END_ORD + 1)
+        if n not in SPECIAL_ORDS
+    ]
+    
+    return nikkud_list + [int(del_flag)]
+
+def haser_male2target(haser, male):
+    '''Input: pairs of texts in ktiv haser (with nikud) and ktiv male
+    Output: multilabel targets for nikud model'''
+    chunked = chunk_haser_male(haser, male)
+    return np.vstack([chunk2target(chunk) for chunk in chunked])
+
+if __name__ == '__main__':
+    haser = 'הַכְּרֻבִים'
+    male = 'הכרובים'
+    print(haser)
+    print(male)
+    print(chunk_haser_male(haser, male))
+    print(haser_male2target(haser, male))
+    print(haser_male2target(haser, male).shape)
