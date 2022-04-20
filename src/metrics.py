be3e005729ee32bf4bfd05544f404f5d9acecb67
Morris Alper <morrisalp@gmail.com>
2022-04-19 22:02:24 -0700

    add masking to unikud metrics

--- src/metrics.py
+++ src/metrics.py
@@ -23,7 +23,7 @@ def ktiv_male_metrics(eval_preds):
         'macro_f1': macro_f1
     }
 
-def unikud_metrics(eval_preds, prob_threshold=0.5):
+def unikud_metrics(eval_preds, prob_threshold=0.5, padding_idx=-100):
     logits, labels = eval_preds
     # shapes of each: (n_samples, n_chars, n_labels=15)
 
@@ -31,6 +31,10 @@ def unikud_metrics(eval_preds, prob_threshold=0.5):
     n_labels = logits.shape[-1]
     logits = logits.reshape(-1, n_labels)
     labels = labels.reshape(-1, n_labels)
+
+    mask = (labels != padding_idx).all(axis=-1)
+    logits = logits[mask]
+    labels = labels[mask]
 
     probs = sigmoid(logits)
     preds = (probs >= prob_threshold).astype(int)
